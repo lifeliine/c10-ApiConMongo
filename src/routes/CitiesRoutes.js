81c82b14e89e8649c2014ce18e3722d6a09e7aff
lifeliine <matias.cespedes@outlook.com>
2021-12-16 18:44:42 -0300

    para tag

--- src/routes/CitiesRoutes.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require('express');
-const {cities} = require('../controlers/CitiesControlers');
-
-const router = Router();
-
-router.get('/:city',cities);
-
-module.exports = router;
